730e1985368668466fed43fcc8d753a9aae8b8c8
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-02 13:14:59 -0400

    created separate folder for provider files and moved TodoProvider.tsx into folder

---
+++ src/providers/TodoProvider.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { TodoContext } from "../context/TodoContext";
+
+type Task = {
+    id: string | number;
+    text: string;
+    completed: boolean;
+}
+
+function TodoProvider({ children }: { children: React.ReactNode }) {
+    // STORE MULTIPLE TODOS IN AN ARRAY
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    // FUNCTION FOR ADDING TODO TASKS
+    function addTask(text: string) {
+        const newTask = {
+            id: crypto.randomUUID(),
+            text: text,
+            completed: false,
+        }
+
+        setTasks((prevTasks) => [...prevTasks, newTask])
+    }
+
+    // FUNCTION FOR TOGGLING COMPLETED TASKS
+    function toggleTask(id: string) {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === id
+                    ? { ...task, completed: !task.completed }
+                    : task)
+        )
+    }
+
+    // FUNCTION TO DELETE A TASK
+    function deleteTask(id: string) {
+        setTasks(prevTasks => prevTasks.filter(task => task.id !== id))
+    };
+
+    // FUNCTION TO EDIT TASK 
+    function editTask(id: string, newText: string) {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === id
+                    ? { ...task, text: newText }
+                    : task
+            )
+        )
+    }
+
+    // FUNCTION TO CLEAR COMPLETED TASKS
+    function clearCompleted() {
+        setTasks((prevTasks) =>
+            prevTasks.filter(
+                (task) => task.completed
+            )
+        )
+    }
+
+    return (
+        <TodoContext.Provider value={{ tasks, addTask, toggleTask, deleteTask, editTask, clearCompleted }}>
+            {children}
+        </TodoContext.Provider>
+    )
+}
+
+export default TodoProvider;
