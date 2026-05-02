f76678699a2e4517371dedb5ba9b76c7ab8a3ff6
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-02 11:49:51 -0400

    changed task useState to an array to hold multiple values; declared newTask variable in addTodo function

--- src/components/TodoProvider.tsx
+++ src/components/TodoProvider.tsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import { TodoContext } from "../context/TodoContext";
 
 function TodoProvider({children}) {
-    const [task, setTask] = useState({
-        id: "",
-        text: "",
-        completed: false,
-    })
+    // STORE MULTIPLE TODOS IN AN ARRAY
+    const [tasks, setTasks] = useState([])
 
-    function addTodo() {
-        const todoList = [];
+    function addTodo(text: string) {
+        const newTask = {
+            id: crypto.randomUUID(),
+            text: text,
+            completed: false,
+        }
 
+        
         
     }
 }
