033ed22ee59a86de9ad46a02949a66d286a215aa
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 11:12:07 -0400

    added useState for theme; created function for toggling theme; exported ThemeProvider function

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -2,5 +2,22 @@ import { useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeProvider({children}: {children: React.ReactNode}) {
-    
+    const [theme, setTheme] = useState("light")
+
+    function toggleTheme() {
+        setTheme(
+            theme === "light"
+            ? "dark"
+            : "light"
+        )
+    }
+
+    return (
+        <ThemeContext.Provider
+        value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
 }
+
+export default ThemeProvider;
