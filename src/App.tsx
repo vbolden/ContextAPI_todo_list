7319320fab0cb16bb08c06b58c45bf6d248c8c7a
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 16:20:13 -0400

    added style for light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,36 @@ import { useContext } from 'react'
 import { ThemeContext } from './context/ThemeContext'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
+import ThemeButton from './components/ThemeButton'
+import FilterButtons from './components/FilterButtons'
 
 
 function App() {
   const themeContext = useContext(ThemeContext);
 
-  if(!themeContext) return null;
+  if (!themeContext) return null;
 
   const theme = themeContext.theme;
 
 
   return (
-    <div>
+    <div className='container'
+      style={{
+        backgroundColor:
+          theme === "light"
+            ? "white"
+            : "#222",
+
+        color:
+          theme === "light"
+            ? "black"
+            : "white",
+      }}
+    >
       <h1>Todo App</h1>
+      <ThemeButton />
       <TodoInput />
+      <FilterButtons />
       <TodoList />
     </div>
   )
