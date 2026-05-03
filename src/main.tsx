0916ffabdd5903e5c1b3f6927563d543dd96e8e2
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 11:26:42 -0400

    imported Todo,Filter, and Theme providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import TodoProvider from './providers/TodoProvider.tsx'
+import FilterProvider from './providers/FilterProvider.tsx'
+import ThemeProvider from './providers/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
