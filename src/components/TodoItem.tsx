6dae5d5997e3cbb53c15f6dfaf789af414d987d1
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 12:59:32 -0400

    added UI to return and imported Todo type alias for props

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,44 @@
 import { useContext } from "react";
 import { TodoContext } from "../context/TodoContext";
+import type { Todo } from "../providers/TodoProvider";
 
-function TodoItem() {
+function TodoItem({todo}: {todo: Todo}) {
     const todoContext = useContext(TodoContext);
 
-    if(!todoContext) return null;
+    if (!todoContext) return null;
+
+    const toggleTodo = todoContext.toggleTodo;
+    const deleteTodo = todoContext.deleteTodo;
+
+    return (
+        <div>
+            <span
+                style={{
+                    textDecoration:
+                        todo.completed
+                            ? "line-through"
+                            : "none"
+                }} >
+                {todo.text}
+            </span>
+
+            <button
+                onClick={() =>
+                    toggleTodo(todo.id)
+                }
+            >
+                Complete
+            </button>
+
+            <button
+                onClick={() =>
+                    deleteTodo(todo.id)
+                }
+            >
+                Delete
+            </button>
+        </div>
+    )
 }
+
+export default TodoItem;
