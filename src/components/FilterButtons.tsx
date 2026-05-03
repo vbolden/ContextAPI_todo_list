d79c4ae32da94f686c50539b5125193513a49e71
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 15:47:38 -0400

    added buttons to UI for all, active, and completed filter

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -2,7 +2,21 @@ import { useContext } from "react";
 import { FilterContext } from "../context/FilterContext";
 
 function FilterButtons() {
-    
+    const filterContext = useContext(FilterContext);
+
+    const filter = filterContext.filter;
+    const setFilter = filterContext.setFilter;
+
+    return (
+        <div>
+            <div>
+                <button onClick={setFilter("all")} >All</button>
+                <button onClick={setFilter("active")} >Active</button>
+                <button onClick={setFilter("completed")} >Completed</button>
+            </div>
+            <p>Current filter: {filter}</p>
+        </div>
+    )
 }
 
 export default FilterButtons;
