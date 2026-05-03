2cb14f22860370a908491ac75843858d1cb4f865
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 16:34:39 -0400

    "added className for styling"

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -8,7 +8,7 @@ function FilterButtons() {
 
     return (
         <div>
-            <div>
+            <div className="filter-buttons">
                 <button onClick={() => setFilter("all")} >All</button>
                 <button onClick={() => setFilter("active")} >Active</button>
                 <button onClick={() => setFilter("completed")} >Completed</button>
