7707170707d827cbd4d868af205bbe45c8897b29
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 14:51:18 -0400

    added if statments for filtering todos by active or completed status

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { TodoContext } from "../context/TodoContext";
 import { FilterContext } from "../context/FilterContext";
+import type { Todo } from "../providers/TodoProvider";
 import TodoItem from "./TodoItem";
 
 function TodoList() {
@@ -13,7 +14,16 @@ function TodoList() {
     const todos = todoContext.todos;
     const filter = filterContext.filter;
 
-    
+    const filteredTodos = todos.filter((todo: Todo) => {
+            if(filter === "active") {
+                return !todo.completed;
+            } 
+            
+            if(filter === "completed") {
+                return todo.completed;
+            }
+            return true;
+        });
 }
 
 export default TodoList;
