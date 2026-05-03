9649652c4282cffa27c209b924b9fcab03ba87a7
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 15:52:41 -0400

    added toggle button

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -2,7 +2,15 @@ import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeButton() {
+    const themeContext = useContext(ThemeContext);
 
+    const toggleTheme = themeContext.toggleTheme;
+
+    return (
+        <button onClick={toggleTheme} >
+            Toggle Theme
+        </button>
+    )
 }
 
 export default ThemeButton;
