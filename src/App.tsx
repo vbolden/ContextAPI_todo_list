1d3bb0e3ee8c8ecb36af3afcc8e6b05b29a40c6e
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 14:59:47 -0400

    added imports and themeContext variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css'
+import { useContext } from 'react'
+import { ThemeContext } from './context/ThemeContext'
 import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
 
 function App() {
+  const themeContext = useContext(ThemeContext);
+
+  if(!themeContext) return null;
 
   return (
     <>
