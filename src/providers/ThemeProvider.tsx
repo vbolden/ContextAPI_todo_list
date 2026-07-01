007d06e0d38d466afb7dc8d7a1aebbc2fcb90514
Valerie Bolden <valerie.bolden@gmail.com>
2026-07-01 15:31:32 -0400

    added theme choice to local storage

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-function ThemeProvider({children}: {children: React.ReactNode}) {
-    const [theme, setTheme] = useState("light")
+function ThemeProvider({ children }: { children: React.ReactNode; }) {
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem("theme") || "light";
+    });
 
     function toggleTheme() {
-        setTheme(
-            theme === "light"
+        const newTheme = theme === "light"
             ? "dark"
-            : "light"
-        )
+            : "light";
+
+        setTheme(newTheme);
+
+        localStorage.setItem("theme", newTheme);
     }
 
     return (
         <ThemeContext.Provider
-        value={{theme, toggleTheme}}>
+            value={{ theme, toggleTheme }}>
             {children}
         </ThemeContext.Provider>
-    )
+    );
 }
 
 export default ThemeProvider;
